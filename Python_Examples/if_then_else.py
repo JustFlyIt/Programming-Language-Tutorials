b3d712ae72b3fa67a10d85a9e547739442290d34
JustFlyIt <DonRSnyder@gmail.com>
2023-01-09 12:36:05 -0500

    New example

--- Python_Examples/if_then_else.py
+++ Python_Examples/if_then_else.py
@@ -1,1 +1,16 @@
-print("test")
+print("Triangle if then else example")
+
+# Scalene triangle: All sides have different lengths.
+# Isosceles triangle: Two sides have the same length.
+# Equilateral triangel: All sides are equal.
+
+a = int(input("The length of side a = "))
+b = int(input("The length of side b = "))
+c = int(input("The length of side c = "))
+
+if a != b and b != c and a != c:
+    print("This is a scalene triangle.")
+elif a == b and b == c:
+    print("This is an equilateral triangle.")
+else:
+    print("This is an isosceles triangle.")
